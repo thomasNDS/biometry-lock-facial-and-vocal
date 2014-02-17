020041ae0e6c24aea41767346aa6b8baefbaf33a
nicko <nicko466@gmail.com>
2014-02-17 13:55:30 +0100

    ajout de fichier et suppresion de certains + ajout de enrolemen.sh et identification.sh les deux seuls sh à utiliser NORMALEMENT

---
+++ vocalRecognitionViaSh/enrolement.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Transform le premier fichier en paramètre (de type en wave) en $1
+java -Xmx2024m -jar ./LIUM_SpkDiarization.jar --fInputMask=./wav/$1.wav --sOutputMask=./seg/$1.seg --doCEClustering  showName
+
+#copy the ubm for each speaker
+java -Xmx2024m -cp LIUM_SpkDiarization.jar fr.lium.spkDiarization.programs.MTrainInit --help --sInputMask=./seg/$1.seg --fInputMask=./wav/$1.wav --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:1:300:4"  --emInitMethod=copy --tInputMask=./ubm.gmm --tOutputMask=$1.init.gmm speakers
+
+#train (MAP adaptation, mean only) of each speaker, the diarization file describes the training data of each speaker.
+java -Xmx2024m -cp LIUM_SpkDiarization.jar fr.lium.spkDiarization.programs.MTrainMAP --help --sInputMask=./seg/$1.seg --fInputMask=./wav/$1.wav --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:1:300:4"  --tInputMask=$1.init.gmm --emCtrl=1,5,0.01 --varCtrl=0.01,10.0 --tOutputMask=./modele/modelede$1.gmm speakers
--- vocalRecognitionViaSh/identification.sh
+++ vocalRecognitionViaSh/identification.sh
@@ -1,1 +1,6 @@
-java -Xmx2G -Xms2G -cp ./LIUM_SpkDiarization.jar  fr.lium.spkDiarization.programs.Identification --help --sInputMask=%s.seg --fInputMask=%s.wav  --sOutputMask=%s.ident.seg --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:1:300:4" --tInputMask=speakers.gmm --sTop=5,ubm.gmm  --sSetLabel=add testShow
+
+
+#Transform le premier fichier en paramètre (de type en wave) en $1
+java -Xmx2024m -jar ./LIUM_SpkDiarization.jar --fInputMask=./wav/$1.wav --sOutputMask=./seg/$1.seg --doCEClustering  showName
+
+java -Xmx2G -Xms2G -cp ./LIUM_SpkDiarization.jar  fr.lium.spkDiarization.programs.Identification --help --sInputMask=./seg/$1.seg --fInputMask=./wav/$1.wav  --sOutputMask=$1.ident.seg --fInputDesc="audio16kHz2sphinx,1:3:2:0:0:0,13,1:1:300:4" --tInputMask=./modele/modeledetest1min.gmm --sTop=5,ubm.gmm  --sSetLabel=add testShow
